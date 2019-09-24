3ba41c241d8c8d939b8e9834c9ed7e63a3429c60
legend <legend@legendmyth.com>
2019-09-24 14:09:21 +0800

    修改协议大小

--- ServerRedirect/Program.cs
+++ ServerRedirect/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace ServerRedirect
 {
@@ -8,6 +9,8 @@ namespace ServerRedirect
     {
         static void Main(string[] args)
         {
+            ThreadPool.SetMinThreads(100,20);
+            ThreadPool.SetMaxThreads(200,30);
             SimpleServerNew simpleServer = new SimpleServerNew();
             simpleServer.OpenServer(12345, 34567);
             Console.ReadKey();
